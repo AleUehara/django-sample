83cd10cc13e69a4800b03b27f43dc5f9ee17ba6f
alexandre.uehara <alexandre.uehara@netshoes.com>
2013-07-25 14:27:16 -0300

    adicionado outras views

--- banco/urls.py
+++ banco/urls.py
@@ -1,7 +1,24 @@
+'''
 from django.conf.urls.defaults import *
 
 from banco import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index')
+)
+'''
+
+from django.conf.urls.defaults import *
+
+from banco import views
+
+urlpatterns = patterns('',
+    # ex: /polls/
+    url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<banco_id>\d+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<banco_id>\d+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    #url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
--- banco/views.py
+++ banco/views.py
@@ -1,4 +1,18 @@
 from django.http import HttpResponse
 
+
+#http://maltzsama.blogspot.com.br/2009/09/acessar-sql-server-com-python.html
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")# Create your views here.
+    return HttpResponse("Hello, world. You're at banco.")# Create your views here.
+
+
+def detail(request, banco_id):
+    return HttpResponse("You're looking at banco %s." % banco_id)
+
+def results(request, banco_id):
+    return HttpResponse("You're looking at the banco %s." % banco_id)
+
+#def vote(request, poll_id):
+#    return HttpResponse("You're voting banco%s." % poll_id)
